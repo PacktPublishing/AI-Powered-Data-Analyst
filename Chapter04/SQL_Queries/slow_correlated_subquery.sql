e3f008f7209da090a4d3ad22d63711b6bdff5fa9
James Gabriel <jamesozgabriel808@gmail.com>
2026-05-17 20:07:59 +0100

    Add slow correlated subquery SQL example

---
+++ Chapter04/SQL_Queries/slow_correlated_subquery.sql
@@ -0,0 +1,8 @@
+SELECT 
+   c.customer_id, 
+   c.email, 
+  (SELECT MAX(order_date) 
+       FROM orders o 
+       WHERE o.customer_id = c.customer_id 
+   ) AS last_order_date 
+FROM customers c; 
